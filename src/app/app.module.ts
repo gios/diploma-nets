a2e6fb30112e3e24e0ac45d214f895da9ca97b8c
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-09-20 15:50:01 +0300

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdSidenavModule, MdButtonModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MdSidenavModule,
+    MdButtonModule,
     AppRoutingModule
   ],
   providers: [],
