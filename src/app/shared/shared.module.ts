5817e025fba13adf69b03a8c0b59fc44eecf3b84
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-09-23 16:34:38 +0300

    sidenav init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NetComponent } from './net/net.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    NetComponent
+    NetComponent,
+    SidenavComponent
   ],
   exports: [
-    NetComponent
+    NetComponent,
+    SidenavComponent
   ]
 })
 export class SharedModule { }
