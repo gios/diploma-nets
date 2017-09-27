fa31f8e0f169699deee30bb202455e88a5934440
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-09-27 20:35:15 +0300

    git keep

--- backend/dist/routes/routes-init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var test_routes_1 = require("./test-routes");
-exports.initRoutes = function (app) {
-    app.use(test_routes_1.router.routes());
-    app.use(test_routes_1.router.allowedMethods());
-};
-//# sourceMappingURL=routes-init.js.map
--- backend/dist/routes/test-routes.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="koa-router" />
-import * as Router from 'koa-router';
-export declare const router: Router;
