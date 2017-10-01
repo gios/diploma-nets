86d6a8550ac0fc5c5fd6c67b9b079289686352f2
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-10-01 23:01:10 +0300

    html5 router support

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import * as bodyparser from 'koa-bodyparser';
 import * as convert from 'koa-convert';
 import * as helmet from 'koa-helmet';
 import * as serve from 'koa-static';
+import * as send from 'koa-send';
 import * as winston from 'winston';
 
 import { initRoutes } from './routes/routes-init';
@@ -13,6 +14,13 @@ const app = new Koa();
 
 app.use(helmet());
 app.use(serve(__dirname + '/../public'));
+app.use(async (ctx, next) => {
+  if (ctx.path.split('/')[1] !== 'api') {
+    await send(ctx, 'index.html', { root: __dirname + '/../public' });
+  } else {
+    await next();
+  }
+});
 app.use(convert(bodyparser()));
 
 // Routes
