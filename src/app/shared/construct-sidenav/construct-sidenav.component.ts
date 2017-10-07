9b41eae61edfa156610bc14d9379829896c4d489
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-10-07 21:07:43 +0300

    up

--- src/app/shared/construct-sidenav/construct-sidenav.component.ts
+++ src/app/shared/construct-sidenav/construct-sidenav.component.ts
@@ -24,53 +24,9 @@ export class ConstructSidenavComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.data && changes.data.currentValue) {
-      const pinnacles = changes.data.currentValue.pinnacles as IPinnacle[];
-      const transitions = changes.data.currentValue.transitions as ITransition[];
-      const connections = changes.data.currentValue.connections as ILinkConnection[];
-      const generatedPinnacles = this.netService.getGeneratedPinnacles(pinnacles);
-      const generatedTransitions = this.netService.getGeneratedTransitions(transitions);
-      const generatedConnections = this.netService.getGeneratedConnections(generatedPinnacles, generatedTransitions, connections);
-      const netEntities = generatedPinnacles.concat(generatedTransitions);
-      generatedConnections.forEach(value => {
-        const source = value.get('source');
-        const target = value.get('target');
-        value.set('sourceItem', find(netEntities, ['id', source.id]));
-        value.set('targetItem', find(netEntities, ['id', target.id]));
-      });
-
-      generatedPinnacles.forEach(pinnacle => {
-        this.navPinnacles.push({
-          id: pinnacle.get('baseId'),
-          name: pinnacle.get('baseName'),
-          value: pinnacle.get('tokens'),
-          x: pinnacle.get('position').x,
-          y: pinnacle.get('position').y
-        });
-      });
-
-      generatedTransitions.forEach(transition => {
-        this.navTransitions.push({
-          id: transition.get('baseId'),
-          name: transition.get('baseName'),
-          time: transition.attr('.label-time/text').split(' ')[2],
-          x: transition.get('position').x,
-          y: transition.get('position').y
-        });
-      });
-
-      generatedConnections.forEach(connect => {
-        this.navConnections.push({
-          id: +connect.id,
-          value: this.netService.getLinkValue(connect),
-          connect: [{
-            type: connect.get('sourceItem').get('connectionType'),
-            name: connect.get('sourceItem').get('baseName')
-          }, {
-            type: connect.get('targetItem').get('connectionType'),
-            name: connect.get('targetItem').get('baseName')
-          }]
-        });
-      });
+      this.navPinnacles = changes.data.currentValue.pinnacles as IPinnacle[];
+      this.navTransitions = changes.data.currentValue.transitions as ITransition[];
+      this.navConnections = changes.data.currentValue.connections as ILinkConnection[];
     }
   }
 
