023bd31d950ce1307ac68f034a9956ba60c4e5f8
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-10-15 13:18:18 +0300

    removed

--- backend/dist/routes/routes-users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Router = require("koa-router");
-var middlewares_1 = require("../users/middlewares");
-exports.router = new Router();
-exports.router.post('/login', middlewares_1.login);
-exports.router.post('/registration', middlewares_1.registration);
-//# sourceMappingURL=routes-users.js.map
