33e9ae07b5b9916f245e1339eb88458c64e83f58
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-10-15 13:11:07 +0300

    heroku push

---
+++ backend/dist/routes/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var routes_nets_1 = require("./routes-nets");
+var routes_users_1 = require("./routes-users");
+exports.routesList = [
+    routes_nets_1.router,
+    routes_users_1.router
+];
+//# sourceMappingURL=routes.js.map
